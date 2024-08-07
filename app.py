8615e138edf0352ababc46fc6128b2cdb226763d
Bernhard-Keller123 <bkeller1@gwdg.de>
2024-08-07 20:09:14 +0500

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from github import Github
 # Greife auf den API-Schlüssel aus der Umgebungsvariable 
 api_key = st.secrets['OPENAI_API']
 github_token = st.secrets['GITHUB_TOKEN']
-repo_name = "Bernhard-Keller123/AventraGPT_MKCG"
+repo_name = "Bernhard-Keller123/AventraGPT_Yogini"
 
 if not api_key:
     st.error("Kein API-Schlüssel gesetzt. Bitte setze die Umgebungsvariable OPENAI_API_KEY.")
@@ -80,7 +80,7 @@ def generiere_antwort(prompt):
         return str(e)
 
 # Streamlit App
-st.title("AventraGPT_MKCG")
+st.title("AventraGPT_Yogini")
 
 # Eingabefeld für den Prompt
 prompt = st.text_input("Du: ")
